f8d648ff62e70dc450f9a9a3708d52fd01d7033f
yusufaltattan <73115671+yusufaltattan@users.noreply.github.com>
2020-12-04 01:26:59 +0300

    Pushing

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from flask import render_template
 import time
 import sched
 import pyttsx3
+import logging
 from datetime import datetime
 from announcement import announce
 from time_conversions import hhmm_to_seconds
@@ -18,39 +19,57 @@ import main_alarm
 app = Flask(__name__)
 
 s = sched.scheduler(time.time, time.sleep)
+logging.basicConfig(level=logging.DEBUG,filename='sys.log')
+
+@app.route('/')
+def hello():
+    return redirect('/index')
 
 @app.route('/index')
 def main():
     '''
     Function for the whole system
     '''
-
-    # Check if notification is sent to be deleted
-    notification = request.args.get('notif')
-    if notification:
-        main_notification.deleteByTitle(notification)
-        return redirect(request.path, code=302)
+    logging.info('Main page accessed.')
     
-    # Check if alarm is sent to be deleted
-    alarm = request.args.get('alarm_item')
-    if alarm:
-        main_alarm.deleteByTitle(alarm)
-        return redirect(request.path, code=302)
+    try:
+        # Check if notification is sent to be deleted
+        notification = request.args.get('notif')
+        if notification:
+            main_notification.deleteByTitle(notification)
+            logging.info('Notification Deleted')
+            return redirect(request.path, code=302)
+    except:
+        logging.error('Error occured while trying to delete notification')
+    
+    try:
+        # Check if alarm is sent to be deleted
+        alarm = request.args.get('alarm_item')
+        if alarm:
+            main_alarm.deleteByTitle(alarm)
+            logging.info('Alarm Deleted')
+            return redirect(request.path, code=302)
+    except:
+        logging.error('Error occured while trying to delete alarm announcement')
+    
 
     '''Function for the announcement alarm'''
 
     s.run(blocking=False)
-   
-    # check if alarm time has been submitted (to create an alarm with notification)
-    alarm_time = request.args.get("alarm")
-    if alarm_time:
-        alarm_label = request.args.get('two')
-        alarm_isNewsIncluded = request.args.get('news')
-        alarm_isWeatherIncluded = request.args.get('weather')
-        # convert to boolean
-        alarm_isNewsIncluded = True if alarm_isNewsIncluded is not None else False
-        alarm_isWeatherIncluded = True if alarm_isWeatherIncluded is not None else False
-        AddAlarm(alarm_time, alarm_label, alarm_isNewsIncluded, alarm_isWeatherIncluded)
+    try:
+        # check if alarm time has been submitted (to create an alarm with notification)
+        alarm_time = request.args.get("alarm")
+        if alarm_time:
+            alarm_label = request.args.get('two')
+            alarm_isNewsIncluded = request.args.get('news')
+            alarm_isWeatherIncluded = request.args.get('weather')
+            # convert to boolean
+            alarm_isNewsIncluded = True if alarm_isNewsIncluded is not None else False
+            alarm_isWeatherIncluded = True if alarm_isWeatherIncluded is not None else False
+            AddAlarm(alarm_time, alarm_label, alarm_isNewsIncluded, alarm_isWeatherIncluded)
+            logging.info('Alarm has been added.')
+    except:
+        logging.error('Error occured while trying to submit alarm')
 
     return render_template('template.html', title='Daily update',
      notifications = main_notification.getNotifications(), 
